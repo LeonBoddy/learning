adb0780d0dec4384df3d707fb99a0a67e3eb8cb4
LeonBoddy <leon.boddy@visualsoft.co.uk>
2022-11-23 11:10:18 +0000

    tailwind

--- vue-learning/vue-components/js/components/AppButton.js
+++ vue-learning/vue-components/js/components/AppButton.js
@@ -1,23 +1,27 @@
 export default {
     template: `
-        <button :class="{
-            'test': true
-        }" :disabled="processing">
+        <button
+             :class="{
+                'border rounded px-5 py-2 :disabled:cursor-not-allowed': true,
+                'btn-blue': type === 'primary',
+                'btn-purple': type === 'secondary',
+                'btn-grey': type === 'muted',
+                'is-loading': processing
+            }" 
+            :disabled="processing"
+        >
             <slot />
         </button>
     `,
-
     props: {
         type: {
             type: String,
             default: 'primary'
+        },
+        processing: {
+            type: Boolean,
+            default: false,
         }
-
     },
 
-    data() {
-        return {
-            processing: false,
-        }
-    }
 }
